4cc1c837890dff3ce5ff6393ecf015a357f8b7e8
NicholasHattrup <nhattrup@andrew.cmu.edu>
2024-02-01 14:33:54 -0500

    Redefined classes given diagram from yesterday, defined a few python subclasses off the top of my head

---
+++ MDPilot/src/Pilot/pilot.py
@@ -0,0 +1,3 @@
+# Core novelty of this library
+# Given an interface with a ScriptComponents object, PostProcess object, and Campaign object
+# Will automatically determine initial and (if needed) future workflows to meet objective of campaign object
